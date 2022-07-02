540163ca8b1f502b8da9739a1e336085ad05d4f7
AsuweRich <pickhimyoung24@yahoo.com>
2022-07-01 21:21:44 -0700

    My c coding commit for variables, if, else, and while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,25 @@
-
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - check if a random number is positive or negative
+ *
+ * Return: 0 if error
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	nLast = n % 10;
+
+	if (nLast > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, nLast);
+	else if (nLast < 6 && nLast != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nLast);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, nLast);
 	return (0);
 }
