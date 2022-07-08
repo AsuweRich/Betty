a32e992a9f368ed971405bf94fcc1d01407e2674
AsuweRich <pickhimyoung24@yahoo.com>
2022-07-08 16:10:59 -0700

    C variables if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,53 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 
 int main(void)
 {
-	int c;
-	int d;
-	int e;
-	int f = 0;
+	int p, q;
 
-	while (f < 10)
+	for (p = 0; p <= 98; p++)
 	{
-		e = 0;
-		while (e < 10)
+		for (q = p + 1; q <= 99; q++)
 		{
-			d = 0;
-			while (d < 10)
-			{
-				c = 0;
-				while (c < 10)
-				{
-					if (!(f == c && e == d))
-					{
-						putchar('0' + f);
-						putchar('0' + e);
-						putchar(' ');
-						putchar('0' + d);
-						putchar('0' + c);
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
-						if (!(f + e == 18 && c + d == 17 && d == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					c++;
-				}
-				d++;
-			}
-			e++;
+			if (p == 98 && q == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		f++;
 	}
+
 	putchar('\n');
 	return (0);
 }
